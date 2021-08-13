df9e9bb068adbb518f57f98a4533f08ad2fb4fbc
Jaypee <jignacio@cambridge.org>
2021-08-13 11:31:16 +0800

    Test initial SPA handling

---
+++ src/routes/[404].svelte
@@ -0,0 +1,5 @@
+
+<div>
+  <h1>Page not found</h1>
+  <a href="/">Back to homepage</a>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		Hom<sup>e</sup>page
+	</h1>
+
+	<h2>
+		projects collection
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+</style>
